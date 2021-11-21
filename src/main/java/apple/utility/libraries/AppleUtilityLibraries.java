e3748bf4a8a99659786cbc115c62513bb223c5c1
amp7368 <47309189+amp7368@users.noreply.github.com>
2021-11-21 01:18:27 -0500

    first commit

---
+++ src/main/java/apple/utility/libraries/AppleUtilityLibraries.java
@@ -0,0 +1,9 @@
+package apple.utility.libraries;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AppleUtilityLibraries extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+}
